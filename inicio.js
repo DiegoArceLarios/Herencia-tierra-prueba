4425da4da516d61ea80045cb6192fe6da9db1dd3
DiegoArceLarios <112732312+DiegoArceLarios@users.noreply.github.com>
2026-03-08 06:33:33 +0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,16 @@
+function alerta() {
+    Swal.fire({
+            title: "¡Bienvenido!",
+            text: "Has sido invitado al Herencia y Tierra",
+            color: "#fff",
+            imageUrl: "./assets/giphy.gif",
+            padding: "2em",
+            background: "rgba(63, 62, 47, 1)",
+            backdrop: "rgba(141, 139, 139, 0.31)",
+            timer: 4000,
+            showConfirmButton: false
+        });
+        }
+console.log("¡Bienvenido al quinto sol!");
+
+alerta();
